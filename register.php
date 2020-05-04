1257009b022e3dc271cc7e6a867b7870cd4b2af2
Sakshi1520 <58680810+Sakshi1520@users.noreply.github.com>
2020-05-05 01:05:27 +0530

    Add files via upload

--- register.php
+++ register.php
@@ -1,22 +1,24 @@
 <?php
 
 session_start();
-// header('location:home.php');
+header('location:quiz.php');
 
 $con = mysqli_connect('localhost','root');
-	if($con){
-		echo"connection";
-	}
+if($con){
+	echo"connection";}
 
-	mysqli_select_db($con,'quizdb');
+mysqli_select_db($con,'quizdb');
 
-	$name = $_POST['name'];
-    $email = $_POST['email'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phno =$_POST['contact'];
+$clgname = $_POST['clgname'];
+$age = $_POST['age'];
+$_SESSION['username']=$name;
 
+
+$q = "INSERT INTO `user`(`name`, `email`, `phno`, `clgname`, `age`) VALUES ('$name', '$email', '$phno', '$clgname', $age)";
+mysqli_query($con,$q);
 	
-
-	 echo $name;
-	 echo $email;
-
-     //we have to store the user details into the datbase
 ?>
+
